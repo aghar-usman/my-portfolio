6cac4b9010d74a2e8e9d5ff8b396aef2e9d6c5d6
Aghar Usman Kannanthodi <agharusman529@gmail.com>
2025-03-24 12:33:52 +0530

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const projectCards = document.querySelectorAll(".project-card");
+
+    // Toggle flip effect on click
+    projectCards.forEach((card) => {
+        card.addEventListener("click", () => {
+            card.classList.toggle("flipped");
+        });
+    });
+});
+
+// Function to open project link in a new tab
 function openProject(url) {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
 }
